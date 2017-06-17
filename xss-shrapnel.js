a430e4cb002361db0cffcc8fac869d762736d8e8
amlnspqr <amnspqr@ya.ru>
2017-06-17 17:55:48 +0300

    Post data

--- xss-shrapnel.js
+++ xss-shrapnel.js
@@ -7,10 +7,17 @@
 // @grant       none
 // ==/UserScript==
 
-var payload = 'aaa"bbb\'ccc<ddd>eee'
-var regex = /aaa.*?eee/g	//	(new XMLSerializer().serializeToString(document)).match (/aaa.*?eee/g)
+var bForceUrlEncoded = 1
+var bFillHiddenForms = 1
 
-window.addEventListener 
+var iPayload = 0
+
+var aPayloads = ['aaa"bbb\'ccc<ddd>eee', 'aaa"bbb\'eee', 'aaa"eee']
+var regex = /aaa.*?eee/g
+
+var sPayload = aPayloads [iPayload]
+
+window.addEventListener
 (
 	'keydown', 
 	function (e)
@@ -18,6 +25,8 @@ window.addEventListener
 		if ((e.altKey || e.modifiers) && (e.target.tagName == 'INPUT' || e.target.tagName == 'TEXTAREA') && e.target.form)
 		{
 			var parentForm = e.target.form
+			
+			bForceUrlEncoded && (parentForm.enctype = 'application/x-www-form-urlencoded')
 			
 			e.keyCode == 83 && submitForm (parentForm)
 			e.keyCode == 65 && fillForm (parentForm) && submitForm (parentForm)
@@ -28,13 +37,18 @@ window.addEventListener
 
 function submitForm (form)
 {
-	confirm ('Submit?') && HTMLFormElement.prototype.submit.call (form)
+	var aPostData = []
+	
+	for (var i = 0; i < form.length; i++)
+		aPostData.push (encodeURIComponent (form [i].name) + '=' + encodeURIComponent (form [i].value))
+		
+	confirm (form.action + '\r\n\r\n' + aPostData.join ('&')) && HTMLFormElement.prototype.submit.call (form)
 }
 
 function fillForm (form)
 {
 	for (var i = 0; i < form.length; i++)
-		form [i].type == 'file' || (form [i].value = payload)
+		form [i].type == 'file' || (form [i].value = sPayload)
 	
 	return 1
 }
